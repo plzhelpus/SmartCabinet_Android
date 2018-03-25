a65dcbb5e621cd2b1cd09cd38be592670041ab53
Donghwan Kim <wraithtorch@gmail.com>
2018-03-25 16:45:09 +0900

    [CabinetRequestOnClickListener.kt] 클릭되었는지 확인하기 위한 로그 추가 [MainActivity.kt] FirebaseUI Auth 예제를 적용(하지만 안됨) [activity_auth_ui.xml] SnackBar를 호출하기 위해 루트 레이아웃을 Coordina torLayout으로 바꿈. [app_bar_main.xml] 루트 레이아웃에 id 추가 [string.xml] Auth UI에 필요한 오류 메세지 추가 [app/build.gradle] Firebase Auth 추가 및 Firebase API끼리 모아둠.

--- app/src/main/java/org/plzhelpus/smartcabinet_android/CabinetRequestOnClickListener.kt
+++ app/src/main/java/org/plzhelpus/smartcabinet_android/CabinetRequestOnClickListener.kt
@@ -1,5 +1,6 @@
 package org.plzhelpus.smartcabinet_android
 
+import android.util.Log
 import android.view.View
 
 /**
@@ -7,8 +8,12 @@ import android.view.View
  *
  * 사물함에 열림/닫힘 등의 요청을 할 때 누르는 버튼의 클릭 이벤트를 처리하는 OnClickListener
 */
+
 class CabinetRequestOnClickListener : View.OnClickListener{
+    private val TAG = "CabinetRequestButton"
+
     override fun onClick(v: View?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Log.d(TAG, "Cabinet request button clicked")
     }
 }
--- app/src/main/java/org/plzhelpus/smartcabinet_android/MainActivity.kt
+++ app/src/main/java/org/plzhelpus/smartcabinet_android/MainActivity.kt
@@ -3,29 +3,50 @@ package org.plzhelpus.smartcabinet_android
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import java.util.*
+import android.support.design.widget.Snackbar
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import com.firebase.ui.auth.ErrorCodes
+import com.google.firebase.auth.FirebaseUser
+
+/**
+ * Created by Donghwan Kim on 2018-03-23.
+ *
+ * 앱의 런쳐 액티비티
+*/
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
+    private val RC_SIGN_IN = 200
+    private val RC_SIGN_IN_FAIL = 403
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val auth : FirebaseAuth = FirebaseAuth.getInstance()
-        if (auth.currentUser == null){
-            TODO("not implemented") // 유저가 로그인하지 않았으므로 FirebaseUI 호출 필요.
-            startActivity(Intent(this, AuthUiActivity::class.java))
-            finish()
-            return;
+        val currentUser : FirebaseUser? = FirebaseAuth.getInstance().currentUser
+
+        if (currentUser == null) {
+            startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                AuthUI.IdpConfig.GoogleBuilder().build()))
+                        .build(),
+                RC_SIGN_IN)
         }
 
         setContentView(R.layout.activity_main)
@@ -36,8 +57,41 @@ class MainActivity : AppCompatActivity() {
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
-        val cabinetRequestButton : Button = findViewById<Button>(R.id.cabinet_request_button)
+        val cabinetRequestButton : Button = findViewById(R.id.cabinet_request_button)
         cabinetRequestButton.setOnClickListener(CabinetRequestOnClickListener())
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // RC_SIGN_IN 은 로그인 절차를 시작할 때 당신이 startActivityForResult(...) 안으로 전해준 요청 코드다.
+        var mContentLayout: View = findViewById(R.id.content_layout)
+        if (requestCode == RC_SIGN_IN) {
+            var response: IdpResponse
+            try{
+                response = IdpResponse.fromResultIntent(data)!!
+            }catch(e: NullPointerException){
+                Snackbar.make(mContentLayout,"sign in cancelled" ,Snackbar.LENGTH_LONG)
+                return
+            }
+            // 성공적으로 로그인
+            if (resultCode == Activity.RESULT_OK){
+                TODO("not implemented") //성공적으로 로그인했을 때의 절차 표시
+            } else {
+                // 로그인 실패
+                if(response.error?.errorCode == ErrorCodes.NO_NETWORK){
+                    Snackbar.make(mContentLayout, "no network", Snackbar.LENGTH_LONG)
+                    return
+                }
+                Snackbar.make(mContentLayout, "unknown error", Snackbar.LENGTH_LONG)
+                Log.e(TAG, "Sign-in error: ", response.error)
+            }
+        }
+    }
+
+    private fun polulateProfile() {
+        var user: FirebaseUser = FirebaseAuth.getInstance().currentUser ?: return
+        var userEmail:TextView = findViewById(R.id.user_email)
+        userEmail.setText( if (TextUtils.isEmpty(user.email)) "No email" else user.email)
     }
 
     override fun onBackPressed() {
