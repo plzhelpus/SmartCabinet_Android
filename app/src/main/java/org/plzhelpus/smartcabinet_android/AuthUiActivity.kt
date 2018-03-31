e5dd47daa54bf6014b512cbcad8feb75a9992aa5
Donghwan Kim <wraithtorch@gmail.com>
2018-03-31 19:43:30 +0900

    사용자 로그인 과정을 수정 - 메인 엑티비티가 처리하지 않고 로그인 용 액티비티를 호출함. [MainActivity.kt] 유저가 로그인 하지 않았다면 AuthUiActivity 호출 [AuthUiActivity.kt] Sign in 버튼을 눌렀을 경우 로그인 처리 구현 [app/build.gradle] firebase와 firebaseUI 버전을 업그레이드함.

--- app/src/main/java/org/plzhelpus/smartcabinet_android/AuthUiActivity.kt
+++ app/src/main/java/org/plzhelpus/smartcabinet_android/AuthUiActivity.kt
@@ -1,12 +1,97 @@
 package org.plzhelpus.smartcabinet_android
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.StringRes
+import android.support.design.widget.Snackbar
+import android.util.Log
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
+import kotlinx.android.synthetic.main.activity_auth_ui.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+import java.util.*
+import com.google.firebase.auth.FirebaseAuth
+
+
 
 class AuthUiActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = "AuthUiActivity"
+        // startActivityForResult의 결과 코드
+        private val RC_SIGN_IN = 200
+
+        fun createIntent(context: Context): Intent{
+            return Intent(context, AuthUiActivity::class.java)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth_ui)
+
+        user_sign_in_button.setOnClickListener{
+            view -> startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                AuthUI.IdpConfig.GoogleBuilder().build()))
+                        .build(),
+                AuthUiActivity.RC_SIGN_IN)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == AuthUiActivity.RC_SIGN_IN) {
+            handleSignInResponse(resultCode, data)
+        } else {
+            showSnackbar(R.string.unknown_response)
+        }
+    }
+
+    private fun handleSignInResponse(resultCode: Int, data: Intent?) {
+        val response: IdpResponse? = IdpResponse.fromResultIntent(data)
+        if(response == null) {
+            showSnackbar(R.string.sign_in_cancelled)
+            return
+        }
+        // 성공적으로 로그인
+        if (resultCode == Activity.RESULT_OK){
+            backToMainActivity(response)
+        } else {
+            // 로그인 실패
+            if(response.error?.errorCode == ErrorCodes.NO_NETWORK) {
+                showSnackbar(R.string.no_internet_connection)
+            } else {
+                showSnackbar(R.string.unknown_error)
+                Log.e(TAG, "Sign-in error: ", response.error)
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val auth = FirebaseAuth.getInstance()
+        if (auth.currentUser != null) {
+            backToMainActivity(null)
+        }
+    }
+
+    private fun backToMainActivity(response: IdpResponse?){
+        startActivity(MainActivity.createIntent(this, response))
+        finish()
+    }
+
+
+    /**
+     * 스낵바를 띄워줌
+     */
+    private fun showSnackbar(@StringRes content: Int) {
+        Snackbar.make(content_layout, content, Snackbar.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/org/plzhelpus/smartcabinet_android/MainActivity.kt
+++ app/src/main/java/org/plzhelpus/smartcabinet_android/MainActivity.kt
@@ -1,25 +1,20 @@
 package org.plzhelpus.smartcabinet_android
 
-import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.support.annotation.StringRes
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
-import java.util.*
-import android.support.design.widget.Snackbar
 import android.text.TextUtils
 import android.util.Log
 import android.widget.ArrayAdapter
-import com.firebase.ui.auth.ErrorCodes
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.android.synthetic.main.nav_drawer.*
@@ -33,8 +28,20 @@ import org.plzhelpus.smartcabinet_android.dummy.DummyCabinet
 */
 
 class MainActivity : AppCompatActivity(), CabinetFragment.OnListFragmentInteractionListener, MemberFragment.OnListFragmentInteractionListener{
-    private val TAG = "MainActivity"
-    private val RC_SIGN_IN = 200
+    private var mIdpResponse: IdpResponse? = null
+
+    companion object {
+        private val TAG = "MainActivity"
+        private val EXTRA_IDP_RESPONSE = "extra_idp_response"
+
+        fun createIntent(context: Context, idpResponse: IdpResponse?): Intent{
+            val startIntent: Intent = Intent()
+            if(idpResponse != null) {
+                startIntent.putExtra(EXTRA_IDP_RESPONSE, idpResponse)
+            }
+            return startIntent.setClass(context, MainActivity::class.java)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,70 +49,48 @@ class MainActivity : AppCompatActivity(), CabinetFragment.OnListFragmentInteract
         setSupportActionBar(toolbar)
 
         val toggle = ActionBarDrawerToggle(
-                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+                this,
+                drawer_layout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
-
-        cabinet_request_button.setOnClickListener{_ ->
-            Log.d(TAG, "Cabinet request button clicked")
-        }
 
         // 유저가 로그인했는지 확인
         val currentUser: FirebaseUser? = FirebaseAuth.getInstance().currentUser
         if (currentUser == null) {
-            user_email.setText(R.string.not_sign_in)
-            startActivityForResult(
-                    AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setAvailableProviders(Arrays.asList(
-                                    AuthUI.IdpConfig.GoogleBuilder().build()))
-                            .build(),
-                    RC_SIGN_IN)
+            handleNotSignIn()
         } else {
             populateProfile()
-            val values = arrayOf("Group A", "Group B", "Group C", "Group D", "Group E", "Group F",
-                    "Group G", "Group H", "Group I", "Group J", "Group K", "Group L", "Group M", "Group N")
-            val groupsAdapter: ArrayAdapter<String> = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values)
-            group_list.adapter = groupsAdapter
+            mIdpResponse = intent.getParcelableExtra(MainActivity.EXTRA_IDP_RESPONSE);
         }
+
+        cabinet_request_button.setOnClickListener{
+            view ->
+            // TODO 구현 시 디버그 값 삭제
+            Log.d(TAG, "Cabinet request button clicked")
+        }
+
+        // TODO 배포 전에 더미데이터 삭제 필요
+        val values = arrayOf(
+                "Group A", "Group B", "Group C", "Group D", "Group E", "Group F",
+                "Group G", "Group H", "Group I", "Group J", "Group K", "Group L",
+                "Group M", "Group N")
+        val groupsAdapter: ArrayAdapter<String> = ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, values)
+        group_list.adapter = groupsAdapter
 
         val groupInfoFragmentPagerAdapter = GroupInfoFragmentPagerAdapter(supportFragmentManager)
         group_pager.adapter = groupInfoFragmentPagerAdapter
+
+
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        // RC_SIGN_IN 은 로그인 절차를 시작할 때 당신이 startActivityForResult(...) 안으로 전해준 요청 코드다.
-        if (requestCode == RC_SIGN_IN) {
-            val response: IdpResponse
-            try{
-                response = IdpResponse.fromResultIntent(data)!!
-            } catch(e: NullPointerException) {
-                showSnackbar(R.string.sign_in_cancelled)
-                return
-            }
-            // 성공적으로 로그인
-            if (resultCode == Activity.RESULT_OK){
-                populateProfile()
-            } else {
-                // 로그인 실패
-                if(response.error?.errorCode == ErrorCodes.NO_NETWORK){
-                    showSnackbar(R.string.no_internet_connection)
-                    return
-                }
-                showSnackbar(R.string.unknown_error)
-                Log.e(TAG, "Sign-in error: ", response.error)
-            }
-        } else {
-            showSnackbar(R.string.unknown_response)
-        }
-    }
-
-    /**
-     * 스낵바를 띄워줌
-     */
-    private fun showSnackbar(@StringRes content: Int) {
-        Snackbar.make(content_layout, content, Snackbar.LENGTH_LONG).show()
+    private fun handleNotSignIn(){
+        startActivity(AuthUiActivity.createIntent(this))
+        finish()
+        return
     }
 
     /**
@@ -113,7 +98,8 @@ class MainActivity : AppCompatActivity(), CabinetFragment.OnListFragmentInteract
      */
     private fun populateProfile() {
         val user: FirebaseUser = FirebaseAuth.getInstance().currentUser ?: return
-        user_email.text = if (TextUtils.isEmpty(user.email)) "No email" else user.email
+        val email = if (TextUtils.isEmpty(user.email)) "No email" else user.email!!
+        user_email.text = email.split(limit = 1, delimiters = '@')[0]
     }
 
     override fun onBackPressed() {
@@ -130,14 +116,7 @@ class MainActivity : AppCompatActivity(), CabinetFragment.OnListFragmentInteract
         // 유저가 로그인 했는지 확인
         val currentUser : FirebaseUser? = FirebaseAuth.getInstance().currentUser
         if (currentUser == null) {
-            user_email.setText(R.string.not_sign_in)
-            startActivityForResult(
-                    AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setAvailableProviders(Arrays.asList(
-                                    AuthUI.IdpConfig.GoogleBuilder().build()))
-                            .build(),
-                    RC_SIGN_IN)
+            handleNotSignIn()
         } else {
             populateProfile()
         }
