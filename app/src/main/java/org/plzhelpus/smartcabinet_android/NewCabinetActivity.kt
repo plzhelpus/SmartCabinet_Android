252f3877ab346a01b9ec9bfd882a5b0c40ab9c25
Donghwan Kim <wraithtorch@gmail.com>
2018-04-05 21:23:02 +0900

    새로운 사물함 등록하는 액티비티 생성 [app] NewCabinetActivity.kt activity_new_cabinet.xml 생성

---
+++ app/src/main/java/org/plzhelpus/smartcabinet_android/NewCabinetActivity.kt
@@ -0,0 +1,12 @@
+package org.plzhelpus.smartcabinet_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NewCabinetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_cabinet)
+    }
+}
