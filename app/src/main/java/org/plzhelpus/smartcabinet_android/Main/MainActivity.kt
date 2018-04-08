3cb5518869043b72691f6d43401217a57675c775
Donghwan Kim <wraithtorch@gmail.com>
2018-04-08 19:43:55 +0900

    그룹 설정 아이콘과 멤버 추가 아이콘 추가

--- app/src/main/java/org/plzhelpus/smartcabinet_android/Main/MainActivity.kt
+++ app/src/main/java/org/plzhelpus/smartcabinet_android/Main/MainActivity.kt
@@ -164,6 +164,7 @@ class MainActivity : AppCompatActivity(),
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         when (item.itemId) {
+            R.id.action_add_member -> return true
             R.id.action_settings -> return true
             else -> return super.onOptionsItemSelected(item)
         }
